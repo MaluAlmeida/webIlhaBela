c901083840a42786d9c0195ef5e65e8cf6d3a41d
MaluAlmeida <malugoncalves2707@gmail.com>
2024-06-19 17:05:17 -0300

    Adicionar arquivos de projeto.

---
+++ AppIlhabela/wwwroot/js/site.js
@@ -0,0 +1,14 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+
+/*criando a função validar */
+function validar() {
+
+        //chama a tela do index
+        window.open('Home/Praias');
+        //fecha a tela login
+        window.close('Home/Index');
+}
